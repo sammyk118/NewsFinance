32e100a3a2153d01e8c2a33c2c4671daff0f0471
Sammy Kroner <sammyk118@gmail.com>
2021-01-08 11:55:42 -0800

    displays current weather data, almost displays the time

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,53 @@
 var searchBtn = $("#searchBtn");
 var searchCity = $("#searchCity");
+var currTime = $("timeOfDay");
+var currDate = $("dateTime");
+var currTemp = $("#temp");
+var currRain = $("chanceOfRain");
+var futureMinTemp = $("minTemp");
+var futureMaxTemp = $("maxTemp");
+var futureRain = $("chanceOfRain2");
 
 var wKey = "dfaa5e58f81db9579a91fe56b2e69d8e";
 
 
 
-function getCurrentWeather(city) {
-    
+function getWeather(city) {
+    var currWURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + wKey;
+    var futureWURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + wKey;
+
+    $.ajax({
+        url: currWURL,
+        method: "GET",
+    }).then(function (response) {
+        // (K − 273.15) × 9/5 + 32 
+        var temp = Math.round(((response.main.temp - 273.15) * (9 / 5) + 32));
+        currTemp.text("Temperature: " + temp);
+        console.log(response);
+    })
+
+    $.ajax({
+        url: futureWURL,
+        method: "GET",
+    }).then(function (response) {
+        console.log(response);
+    })
 }
 
+function dateAndTime() {
+    var rawDate = new Date();
+    rawDate = String(rawDate);
+    console.log(rawDate);
+    console.log(typeof rawDate);
+    var prettyDate = rawDate.split(" ")[4] + " " + rawDate.split(" ")[1] + ;
+    console.log(prettyDate);
+}
 
 searchBtn.on("click", function (event) {
     var city = "";
     city = searchCity.val().trim();
     console.log(city);
+    getWeather(city);
 });
 
+dateAndTime();
