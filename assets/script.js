557d13dad908795f08c6e2bc54c595584c5d7e44
Shaun Limbeek <soccercomet@yahoo.com>
2021-01-08 12:16:52 -0800

    re-order the script file to fix merge issues

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,3 @@
-function populateCrypto (event) {
-    event.preventDefault();
-    var apikey = "8e3a7aa0-9ee8-452e-aaf7-fabcc9ed8aae";
-    var cryptoURL = "https://cors-anywhere.herokuapp.com/http://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY="+apikey;
-    var cryptoInput = $("#cryptoInput").val().trim();
-    console.log(cryptoURL);
-    $("#cryptoName").text(cryptoInput.toUpperCase());
-
-    $.ajax({
-        url: cryptoURL,
-        method: "GET",
-        cors: true
-    }).then(function (response) {
-        
-        console.log(response.data[0].symbol);
-        console.log(cryptoInput);
-        for (var i=0; i < response.data.length; i++) {
-           if (response.data[i].symbol.toLowerCase() == cryptoInput.toLowerCase()) {
-            $("#cryptoPrice").text("$"+response.data[i].quote.USD.price.toFixed(2));    
-           }
-        }
-        
-    });
-    
-}
-
-$(document).on("click", "#cryptoSearch", populateCrypto);
-
 var searchBtn = $("#searchBtn");
 var searchCity = $("#searchCity");
 var currTime = $("timeOfDay");
@@ -79,3 +51,31 @@ searchBtn.on("click", function (event) {
 });
 
 dateAndTime();
+
+function populateCrypto (event) {
+    event.preventDefault();
+    var apikey = "8e3a7aa0-9ee8-452e-aaf7-fabcc9ed8aae";
+    var cryptoURL = "https://cors-anywhere.herokuapp.com/http://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY="+apikey;
+    var cryptoInput = $("#cryptoInput").val().trim();
+    console.log(cryptoURL);
+    $("#cryptoName").text(cryptoInput.toUpperCase());
+
+    $.ajax({
+        url: cryptoURL,
+        method: "GET",
+        cors: true
+    }).then(function (response) {
+        
+        console.log(response.data[0].symbol);
+        console.log(cryptoInput);
+        for (var i=0; i < response.data.length; i++) {
+           if (response.data[i].symbol.toLowerCase() == cryptoInput.toLowerCase()) {
+            $("#cryptoPrice").text("$"+response.data[i].quote.USD.price.toFixed(2));    
+           }
+        }
+        
+    });
+    
+}
+
+$(document).on("click", "#cryptoSearch", populateCrypto);
