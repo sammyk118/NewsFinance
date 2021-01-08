0543b6f1539ec9023443443e258bf284fca42a0f
Sammy Kroner <sammyk118@gmail.com>
2021-01-08 13:38:40 -0800

    weather forecast

--- assets/script.js
+++ assets/script.js
@@ -13,17 +13,15 @@ var wKey = "dfaa5e58f81db9579a91fe56b2e69d8e";
 
 
 function getWeather(city) {
-    var currWURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + wKey;
-    var futureWURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + wKey;
+    var currWURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + wKey + "&units=imperial";
+    var futureWURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + wKey + "&units=imperial";
 
     $.ajax({
         url: currWURL,
         method: "GET",
     }).then(function (response) {
-        // (K − 273.15) × 9/5 + 32 
-        var temp = Math.round(((response.main.temp - 273.15) * (9 / 5) + 32));
-        currTemp.text("Temperature: " + temp);
-        console.log(response);
+        currTemp.text("Temperature: " + response.main.temp);
+        // console.log(response);
     })
 
     $.ajax({
@@ -31,17 +29,25 @@ function getWeather(city) {
         method: "GET",
     }).then(function (response) {
         console.log(response);
+        for (i = 0; i < response.list.length; i++){
+            if (response.list[i].dt_txt.split(" ")[1] == "12:00:00") {
+                futureMinTemp.text("Min: " + response.list[i].main.temp_min);
+                futureMaxTemp.text("Max: " + response.list[i].main.temp_max);
+                futureRain.text("Rain Chance: " + response.list[i].pop);
+                break;
+            }
+        }
     })
 }
 
 function dateAndTime() {
     var rawDate = new Date();
     rawDate = String(rawDate);
-    console.log(rawDate);
+    // console.log(rawDate);
     var timeData = rawDate.split(" ")[4];
     timeData = timeData.split(":")[0] + ":" + timeData.split(":")[1];
     var prettyDate = timeData + ", " + rawDate.split(" ")[0] + " " + rawDate.split(" ")[1] + " " + rawDate.split(" ")[2];
-    console.log(prettyDate);
+    // console.log(prettyDate);
     currDate.text(prettyDate);
 
     if (timeData.split(":")[0] < 10) {
@@ -58,7 +64,7 @@ function dateAndTime() {
 searchBtn.on("click", function (event) {
     var city = "";
     city = searchCity.val().trim();
-    console.log(city);
+    // console.log(city);
     getWeather(city);
 });
 
