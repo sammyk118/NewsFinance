d16e4f4971f33cb145b9deec0fe8f2d9a9f0b37f
Shaun Limbeek <soccercomet@yahoo.com>
2021-01-11 10:59:45 -0800

    creation of news API, still need to figure out how to get the thumbnail image to work

--- assets/script.js
+++ assets/script.js
@@ -262,3 +262,26 @@ searchStock.on('click', function(){
     getStock(ticker);
 });
 
+
+// Create API pull for top story from NYT
+
+function topNews () {
+    
+    var newsURL = "https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=AU7INFiCVrpNoBfLn4anuAftA5AfsHf2";
+
+    $.ajax({
+        url: newsURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response.results[0]);
+        $("#newsTitle").text(response.results[0].title);
+        // $("#newsThumb").attr("src", response.results[0].media[0].media-metadata[0].url);
+        $("#newsThumb").attr("alt", response.results[0].media[0].caption);
+        $("#storyDetail").text(response.results[0].abstract);
+        $("#storyLink").attr("href", response.results[0].url);
+
+    })
+}
+
+topNews();
+
